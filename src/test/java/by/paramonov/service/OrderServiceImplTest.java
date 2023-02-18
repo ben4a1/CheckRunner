e23a1a206b0adf06177985f3aa098c8f3ea6d592
Alexey Paramonov <paramonov_tama@mail.ru>
2023-02-18 23:42:57 +0300

    style: rename method in OrderServiceImplTest

--- src/test/java/by/paramonov/service/OrderServiceImplTest.java
+++ src/test/java/by/paramonov/service/OrderServiceImplTest.java
@@ -4,10 +4,7 @@ import by.paramonov.entity.Order;
 import by.paramonov.entity.Product;
 import by.paramonov.model.incomeentries.ArgumentEntry;
 import by.paramonov.price.PriceReader;
-import by.paramonov.price.impl.DbPriceReader;
-import by.paramonov.repository.ProductRepository;
 import by.paramonov.service.impl.OrderServiceImpl;
-import by.paramonov.service.impl.ProductServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -21,22 +18,17 @@ import static by.paramonov.util.TestClass.productEntry;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
-
-
     @Mock
     PriceReader mockitoPriceReader;
-
     @BeforeEach
     public void init(){
         MockitoAnnotations.openMocks(this);
     }
     @Test
-    void testCreateOrder() {
-
+    void checkCreateOrderShouldReturnEquals() {
         List<Product> mockitoProductList = new ArrayList<>();
         mockitoProductList.add(new Product(0, "first item", 12.4));
         Mockito.when(mockitoPriceReader.getPriceList()).thenReturn(mockitoProductList);
-
         List<ArgumentEntry> argumentEntryList = new LinkedList<>();
         argumentEntryList.add(productEntry);
         argumentEntryList.add(cardEntry);
@@ -54,5 +46,4 @@ class OrderServiceImplTest {
 
         assertEquals(orderActual.getInputOrder(), orderExpected.getInputOrder());
     }
-
 }
