d003056d6c2d82a39cb945c297b6496f9c9d4b09
Alexey Paramonov <paramonov_tama@mail.ru>
2023-02-19 12:20:41 +0300

    feat: builders classes for tests (start)

---
+++ src/test/java/by/paramonov/util/PriceBuilder.java
@@ -0,0 +1,11 @@
+package by.paramonov.util;
+
+import by.paramonov.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriceBuilder {
+    private List<Product> productList = new ArrayList<>();
+
+}
