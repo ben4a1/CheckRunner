bb07be3a9ce053718d83beb1fc7283a8190b195a
Alexey Paramonov <paramonov_tama@mail.ru>
2023-02-27 10:17:24 +0300

    feat: regex for phone number (customer) in proxt customer service

--- src/main/java/by/paramonov/service/impl/ProxyCustomerServiceImpl.java
+++ src/main/java/by/paramonov/service/impl/ProxyCustomerServiceImpl.java
@@ -1,18 +1,20 @@
 package by.paramonov.service.impl;
 
 import by.paramonov.cache.Cache;
-import by.paramonov.dao.Dao;
 import by.paramonov.entity.Customer;
 import by.paramonov.factory.CacheFactory;
 import by.paramonov.service.CustomerService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class ProxyCustomerServiceImpl implements CustomerService {
     private final Cache<Long, Customer> customerCache;
     private final CustomerService customerService = new CustomerServiceImpl();
+    private final static Pattern PHONE_PATTERN = Pattern.compile("\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}");
 
     public ProxyCustomerServiceImpl(CacheFactory<Long, Customer> customerCacheFactory) {
         this.customerCache = customerCacheFactory.createCache();
@@ -20,8 +22,12 @@ public class ProxyCustomerServiceImpl implements CustomerService {
 
     @Override
     public void addCustomer(Customer customer) {
-        customerService.addCustomer(customer);
-        customerCache.put(customer.getId(), customer);
+        if (isValidPhoneNumber(customer.getPhoneNumber())) {
+            customerService.addCustomer(customer);
+            customerCache.put(customer.getId(), customer);
+        } else {
+            throw new IllegalArgumentException("not valid phone number");
+        }
     }
 
     @Override
@@ -41,12 +47,21 @@ public class ProxyCustomerServiceImpl implements CustomerService {
 
     @Override
     public void update(Customer customer) {
-        customerService.update(customer);
-        customerCache.put(customer.getId(), customer);
+        if (isValidPhoneNumber(customer.getPhoneNumber())) {
+            customerService.update(customer);
+            customerCache.put(customer.getId(), customer);
+        } else {
+            throw new IllegalArgumentException("not valid phone number");
+        }
     }
 
     @Override
     public List<Customer> getAll() {
         return customerService.getAll();
+    }
+
+    private static boolean isValidPhoneNumber(String phoneNUmber) {
+        Matcher matcher = PHONE_PATTERN.matcher(phoneNUmber);
+        return matcher.matches();
     }
 }
