a28dc02b204bb4c8edb82738e8b4a60a403de159
Alexey Paramonov <paramonov_tama@mail.ru>
2023-02-27 11:06:25 +0300

    style: remove 2 imports in CacheFactoryImpl

--- src/main/java/by/paramonov/factory/impl/CacheFactoryLRU.java
+++ src/main/java/by/paramonov/factory/impl/CacheFactoryLRU.java
@@ -4,8 +4,6 @@ import by.paramonov.cache.Cache;
 import by.paramonov.cache.impl.LRUCache;
 import by.paramonov.factory.CacheFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
